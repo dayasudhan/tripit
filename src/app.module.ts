378345da7ea5a8c7c616807312fcbe8e88ef1a76
dayasudhan <dayasudhankg@gmail.com>
2023-02-06 19:53:31 +0530

    mongodb+employee

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { EmployeeModule } from './employee/employee.module';
+const MONGOURI = 
+process.env.ATLAS_URISTRING ||
+  'mongodb://127.0.0.1:27017/Employee';
 @Module({
-  imports: [],
+  imports: [ MongooseModule.forRoot(MONGOURI),EmployeeModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/dto/create-employee.dto.ts
@@ -0,0 +1,5 @@
+import {
+  Employee
+} from "../entities/employee.entity";
+
+export class CreateEmployeeDto extends Employee {}
